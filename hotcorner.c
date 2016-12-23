efce0e4a57d167af37329a65f05d393e2ba5b29c
Ahmed Samy <asamy@protonmail.com>
2016-12-23 14:50:30 +0200

    Improved key state and added hotkeys for closing/temporary disable
    This should help a little, I am not sure if the hotkeys are actually suitably
    aligned...
    
    Following hotkeys are new:
        LCTRL+LALT
            +A => toggle
            +C => shut down

--- hotcorner.c
+++ hotcorner.c
@@ -1,11 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <stdlib.h>
+#include <stdbool.h>
 #include <windows.h>
 
 #pragma comment(lib, "USER32")
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS")
-
-#define KEYDOWN(k) ((k) & 0x80)
 
 // This is a **very** minimal hotcorner app, written in C. Maybe its not the
 // optimal way to do this, but it works for me.
@@ -40,30 +39,26 @@ static const INPUT kCornerInput[] = {
 static const DWORD kHotDelay = 300;
 
 static HANDLE CornerThread = INVALID_HANDLE_VALUE;
+static BYTE KeyState[256];
+static BYTE disabled = 0;
 
 // This thread runs when the cursor enters the hot corner, and waits to see if the cursor stays in the corner.
 // If the mouse leaves while we're waiting, the thread is just terminated.
 static DWORD WINAPI CornerHotFunc(LPVOID lpParameter)
 {
-    BYTE KeyState[256];
     POINT Point;
 
     Sleep(kHotDelay);
 
     // Check if a mouse putton is pressed, maybe a drag operation?
-    if (GetKeyState(VK_LBUTTON) < 0 || GetKeyState(VK_RBUTTON) < 0 || GetKeyState(VK_MBUTTON < 0)) {
+    if (KeyState[VK_LBUTTON] || KeyState[VK_RBUTTON] || KeyState[VK_MBUTTON])  {
         return 0;
-    }
-    
-    // Check if any key is pressed.
-    if (GetKeyboardState(KeyState) == FALSE) {
-        return 1;
     }
 
     // Check if any modifier keys are pressed.
-    if (KEYDOWN(KeyState[VK_SHIFT]) || KEYDOWN(KeyState[VK_CONTROL])
-     || KEYDOWN(KeyState[VK_MENU]) || KEYDOWN(KeyState[VK_LWIN])
-     || KEYDOWN(KeyState[VK_RWIN])) {
+    if (KeyState[VK_SHIFT] || KeyState[VK_CONTROL]
+        || KeyState[VK_MENU] || KeyState[VK_LWIN]
+        || KeyState[VK_RWIN]) {
         return 0;
     }
 
@@ -85,9 +80,38 @@ static DWORD WINAPI CornerHotFunc(LPVOID lpParameter)
 static LRESULT CALLBACK MouseHookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
     MSLLHOOKSTRUCT *evt = (MSLLHOOKSTRUCT *)(lParam);
-
-    if (wParam != WM_MOUSEMOVE)
+    if (disabled)
         goto finish;
+
+    switch (wParam) {
+    case WM_LBUTTONDOWN:
+        KeyState[1] = true;
+        break;
+    case WM_LBUTTONUP:
+        KeyState[1] = false;
+        break;
+    case WM_RBUTTONDOWN:
+        KeyState[2] = true;
+        break;
+    case WM_RBUTTONUP:
+        KeyState[2] = false;
+        break;
+    case WM_MOUSEWHEEL:
+        KeyState[3] = HIWORD(wParam) >= 0 || LOWORD(wParam) == MK_MBUTTON;
+        break;
+    case WM_XBUTTONDOWN:
+        if (HIWORD(evt->mouseData) == XBUTTON1)
+            KeyState[4] = true;
+        else
+            KeyState[5] = true;
+        break;
+    case WM_XBUTTONUP:
+        if (HIWORD(evt->mouseData) == XBUTTON2)
+            KeyState[4] = false;
+        else
+            KeyState[5] = false;
+        break;
+    }
 
     // Check if the cursor is hot or cold.
     if (!PtInRect(&kHotCorner, evt->pt)) {
@@ -125,20 +149,48 @@ finish:
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
+static LRESULT CALLBACK KeyHookCallback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    KBDLLHOOKSTRUCT *evt = (KBDLLHOOKSTRUCT *)lParam;
+    int vkCode;
+    if (nCode != HC_ACTION)
+        goto finish;
+
+    vkCode = evt->vkCode;
+    if (vkCode >= 0x04 && vkCode <= 0x06)
+        --vkCode;
+
+    KeyState[vkCode] = wParam == WM_SYSKEYDOWN || wParam == WM_KEYDOWN;
+    if (KeyState[VK_LCONTROL] && KeyState[VK_LMENU]) { /* LCTRL + LALT  */
+        if (KeyState[0x41])     /* +A => toggle  */
+            disabled = !disabled;
+        else if (KeyState[0x43])    /* +C => die  */
+            TerminateProcess(GetCurrentProcess(), 0);
+    }
+
+finish:
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     MSG Msg;
-    HHOOK Hook;
+    HHOOK MouseHook, KeyHook;
 
 
-    if (!(Hook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookCallback, NULL, 0)))
+    if (!(MouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseHookCallback, NULL, 0)))
         return 1;
+
+    if (!(KeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyHookCallback, NULL, 0)))
+        goto out;
 
     while (GetMessage(&Msg, NULL, 0, 0)) {
         DispatchMessage(&Msg);
     }
 
-    UnhookWindowsHookEx(Hook);
+    UnhookWindowsHookEx(KeyHook);
+out:
+    UnhookWindowsHookEx(MouseHook);
 
     return Msg.wParam;
 }
